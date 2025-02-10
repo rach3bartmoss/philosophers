a11214b3d83bd3d587fe775fb76eec1e967efdb3
Douglas Alves <rachebartm00s@proton.me>
2025-02-10 19:07:23 +0000

    init

--- init_data.c
+++ init_data.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 21:11:25 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/09 22:12:53 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/10 18:55:36 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	init_data(t_data *data, int ac, char **av)
 {
-	data->n_philos = atoi(av[1]);
-	data->time_to_die = atoi(av[2]);
-	data->time_to_eat = atoi(av[3]);
-	data->time_to_sleep = atoi(av[4]);
+	data->n_philos = ft_atoi(av[1]);
+	data->time_to_die = ft_atoi(av[2]);
+	data->time_to_eat = ft_atoi(av[3]);
+	data->time_to_sleep = ft_atoi(av[4]);
 	if (ac == 6)
-		data->n_of_times_philos_eat = atoi(av[5]);
+		data->n_of_times_philos_eat = ft_atoi(av[5]);
 }
