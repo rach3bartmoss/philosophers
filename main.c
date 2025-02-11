6ad2451eb68201c4fd3c9ecdf34eb42dc625c8b0
Douglas Alves <rachebartm00s@proton.me>
2025-02-11 21:27:23 +0000

    3

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 21:02:27 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/09 22:16:48 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/10 22:03:01 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,44 @@
 
 int	main(int ac, char **av)
 {
+	t_data		data = {0};
+
+	init_data(&data, ac, av);
+
+	int			i;
+	int			thread_args[data.n_philos];
+	int			result_code;
+	pthread_t	threads[data.n_philos];
+
 	if (ac < 4)
 	{
 		printf("Error\nInsuficient Arguments\n");
 		return (0);
 	}
 
-	t_data	data = {0};
-
-	int	i = 1;
-	while (i < ac)
+	i = 0;
+	while (i < data.n_philos)
 	{
-		printf("%d\n", atoi(av[i]));
+		thread_args[i] = i;
+		result_code = pthread_create(&threads[i], NULL,
+			thread_function, (void *)&thread_args[i]);
+		if (result_code != 0)
+		{
+			fprintf(stderr, "Error creating thread %d: %d\n", i, result_code);
+			exit(EXIT_FAILURE);
+		}
 		i++;
 	}
-	init_data(&data, ac, av);
-
-	printf("%d\n", data.n_philos);
-	printf("%d\n", data.time_to_die);
-	printf("%d\n", data.time_to_eat);
-	printf("%d\n", data.time_to_sleep);
-	printf("%d\n", data.n_of_times_philos_eat);
+	i = 0;
+	while (i < data.n_philos)
+	{
+		result_code = pthread_join(threads[i], NULL);
+		if (result_code != 0)
+		{
+			fprintf(stderr, "Error joining thread %d: %d\n", i, result_code);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+	printf("All threads have completed execution.\n");
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 21:01:41 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/10 18:55:11 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/11 21:05:40 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,40 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <pthread.h>
+# include <aio.h>
+# include <time.h>
 
-enum e_state
+typedef enum e_state
 {
 	THINKING = 0,
 	SLEEPING = 1,
 	EATING = 2,
 	DEAD = 3,
-};
+}			t_status;
 
 typedef struct s_data
 {
-	int				n_philos;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				n_of_times_philos_eat;
-	enum e_state	state;
+	int			n_philos;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			n_of_times_philos_eat;
+	long		start_time_ms;
+	t_status	status;
 }				t_data;
+
+typedef struct s_list
+{
+	t_data			data;
+	pthread_mutex_t	fork;
+	struct s_list	*prev;
+	struct s_list	*next;
+}				t_list;
 
 void	init_data(t_data *data, int ac, char **av);
 //utils.c
-int	ft_atoi(const char *str);
+int		ft_atoi(const char *str);
+//routines.c
+void	*thread_function(void *arg);
 
 #endif
