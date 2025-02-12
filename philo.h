da8ed13fd110746042e316e2f7420e69c88aedd1
Douglas Alves <rachebartm00s@proton.me>
2025-02-12 20:56:56 +0000

    added on the t_data variables to help the time control management, the printing of messages protect with mutex to avoid many philosopher to print incorrect or out of order messages, current adding to struct many itens to do the job, this could be later change for simplicity and maintanace

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 21:01:41 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/11 23:05:21 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/12 20:53:00 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 # include <pthread.h>
 # include <aio.h>
 # include <time.h>
+# include <stdbool.h>
+# include <sys/time.h>
 
 typedef enum e_state
 {
@@ -29,15 +31,17 @@ typedef enum e_state
 
 typedef struct s_data
 {
-	int			n_philos;
-	int			time_to_die;
-	int			time_to_eat;
-	int			time_to_sleep;
-	int			n_of_times_philos_eat;
-	long		start_time_ms;
-	pthread_t	thread_id;
-	int			philo_id;
-	t_status	status;
+	int				n_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	long			last_meal_time;
+	int				time_to_sleep;
+	int				n_of_times_philos_eat;
+	long			start_time_ms;
+	pthread_t		thread_id;
+	pthread_mutex_t	*print_message;
+	int				philo_id;
+	t_status		status;
 }				t_data;
 
 typedef struct s_list
@@ -49,8 +53,13 @@ typedef struct s_list
 }				t_list;
 
 void	init_main_data(t_data *data, int ac, char **av);
+t_data	copy_data(t_data *data);
+t_list	*create_node(t_data *data);
+void	create_circularll_philos(t_list *head, t_data *data, int n);
 //utils.c
 int		ft_atoi(const char *str);
+long	get_current_time_ms(void);
+void	print_message(t_data *data, const char *action);
 //routines.c
 void	*thread_function(void *arg);
 
