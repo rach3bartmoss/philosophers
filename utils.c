1df32eb783cf9ab6a3c4d6beb010815139febf4e
Douglas Alves <rachebartm00s@proton.me>
2025-02-12 22:19:15 +0000

    utility and helper functions are defined here

---
+++ utils.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 18:53:26 by dopereir          #+#    #+#             */
+/*   Updated: 2025/02/12 21:48:26 by dopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	sign = 1;
+	result = 0;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
+
+long	get_current_time_ms(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+void	print_message(t_data *data, const char *action)
+{
+	pthread_mutex_lock(data->print_message);
+	printf("%ld %d %s\n",
+		get_current_time_ms() - data->start_time_ms,
+		data->philo_id, action);
+	pthread_mutex_unlock(data->print_message);
+}
+
+long	get_elapsed_time(long start_time_ms)
+{
+	return (get_current_time_ms() - start_time_ms);
+}
