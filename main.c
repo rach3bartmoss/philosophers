e414fd947e87950eb5cce3b1302e23d113e5505f
Douglas Alves <rachebartm00s@proton.me>
2025-02-13 21:25:27 +0000

    tests began

--- main.c
+++ main.c
@@ -6,52 +6,75 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 21:02:27 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/11 22:43:21 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/13 21:11:21 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static bool	validate_input(int ac, char **av)
+{
+	int	i;
+	int	value;
+
+	if (ac != 5 && ac != 6)
+	{
+		printf("Error\nInvalid number of arguments\n");
+		return (false);
+	}
+	i = 1;
+	while (i < ac)
+	{
+		value = ft_atoi(av[i]);
+		if (value <= 0)
+		{
+			printf("Error\nAll arguments must be positive numbers\n");
+			return (false);
+		}
+		i++;
+	}
+	if (ft_atoi(av[1]) > 200)
+	{
+		printf("Error\nMaximum number of philosophers is 200\n");
+		return (false);
+	}
+	return (true);
+}
+
 int	main(int ac, char **av)
 {
-	t_data		data = {0};
+	t_data			*main_data;
+	t_list			*head;
+	pthread_mutex_t	print_mutex;
 
-	init_main_data(&data, ac, av);
-
-	int			i;
-	int			thread_args[data.n_philos];
-	int			result_code;
-	pthread_t	threads[data.n_philos];
-
-	if (ac < 4)
+	if (!validate_input(ac, av))
+		return (1);
+	if (pthread_mutex_init(&print_mutex, NULL) != 0)
 	{
-		printf("Error\nInsuficient Arguments\n");
-		return (0);
+		printf("Error\nFailed to initialize print mutex\n");
+		return (1);
 	}
-
-	i = 0;
-	while (i < data.n_philos)
+	main_data = malloc(sizeof(t_data));
+	if (!main_data)
 	{
-		thread_args[i] = i;
-		result_code = pthread_create(&threads[i], NULL,
-			thread_function, (void *)&thread_args[i]);
-		if (result_code != 0)
-		{
-			fprintf(stderr, "Error creating thread %d: %d\n", i, result_code);
-			exit(EXIT_FAILURE);
-		}
-		i++;
+		pthread_mutex_destroy(&print_mutex);
+		return (1);
 	}
-	i = 0;
-	while (i < data.n_philos)
+	init_main_data(main_data, ac, av);
+	main_data->print_message = &print_mutex;
+	head = create_node(main_data);
+	if (!head)
 	{
-		result_code = pthread_join(threads[i], NULL);
-		if (result_code != 0)
-		{
-			fprintf(stderr, "Error joining thread %d: %d\n", i, result_code);
-			exit(EXIT_FAILURE);
-		}
-		i++;
+		cleanup_all(NULL, &print_mutex, main_data);
+		return (1);
 	}
-	printf("All threads have completed execution.\n");
+	create_circularll_philos(head, main_data, main_data->n_philos);
+	if (!init_philos_threads(head, main_data->n_philos))
+	{
+		cleanup_all(head, &print_mutex, main_data);
+		return (1);
+	}
+	cleanup_threads(head, main_data->n_philos);
+	cleanup_all(head, &print_mutex, main_data);
+	return (0);
 }
