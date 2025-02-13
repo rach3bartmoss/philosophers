27ba63b321fdb782397f97b41e9c05bacab8f5e2
Douglas Alves <rachebartm00s@proton.me>
2025-02-13 22:32:37 +0000

    testing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 21:02:27 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/13 21:11:21 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/13 22:21:16 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ int	main(int ac, char **av)
 {
 	t_data			*main_data;
 	t_list			*head;
+	bool			simulation_stop;
+	pthread_mutex_t	stop_mutex;
 	pthread_mutex_t	print_mutex;
 
 	if (!validate_input(ac, av))
@@ -54,12 +56,21 @@ int	main(int ac, char **av)
 		printf("Error\nFailed to initialize print mutex\n");
 		return (1);
 	}
+	if (pthread_mutex_init(&stop_mutex, NULL) != 0)
+	{
+		printf("Error\nFailed to initialize stop_mutex\n");
+		return (1);
+	}
+	simulation_stop = false;
 	main_data = malloc(sizeof(t_data));
 	if (!main_data)
 	{
 		pthread_mutex_destroy(&print_mutex);
+		pthread_mutex_destroy(&stop_mutex);
 		return (1);
 	}
+	main_data->simulation_stop = &simulation_stop;
+	main_data->stop_mutex = &stop_mutex;
 	init_main_data(main_data, ac, av);
 	main_data->print_message = &print_mutex;
 	head = create_node(main_data);
