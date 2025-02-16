17a9a69a877c773d266a54bfc60d4337048537b2
Douglas Alves <rachebartm00s@proton.me>
2025-02-16 22:28:50 +0000

    ++

---
+++ routines_helper.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routines_helper.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/16 22:20:20 by dopereir          #+#    #+#             */
+/*   Updated: 2025/02/16 22:22:31 by dopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	one_philo_handler(t_list *philo)
+{
+	print_message(&philo->data, "has taken a fork");
+	usleep(philo->data.time_to_die * 1000);
+	print_message(&philo->data, "died");
+	stop_simulation(philo);
+}
