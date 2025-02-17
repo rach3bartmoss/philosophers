136c844c11fecb184d98f44fe1cc652e5055281c
Douglas Alves <rachebartm00s@proton.me>
2025-02-17 01:05:59 +0000

    maybe substitute other two functions

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 22:02:40 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/13 22:23:51 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/17 00:49:09 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,24 @@ void	stop_simulation(t_list *philo)
 	pthread_mutex_lock(philo->data.stop_mutex);
 	*philo->data.simulation_stop = true;
 	pthread_mutex_unlock(philo->data.stop_mutex);
+}
+
+bool	check_and_handle_death(t_list *philo)
+{
+	if (get_elapsed_time(philo->data.last_meal_time) > philo->data.time_to_die)
+	{
+		pthread_mutex_lock(philo->data.stop_mutex);
+		if (!*philo->data.simulation_stop)
+		{
+			*philo->data.simulation_stop = true;
+			pthread_mutex_unlock(philo->data.stop_mutex);
+			print_message(&philo->data, "died");
+		}
+		else
+		{
+			pthread_mutex_unlock(philo->data.stop_mutex);
+		}
+		return (true);
+	}
+	return (false);
 }
