aea083238ef2e9346523af421ef6bde6003dd415
Douglas Alves <rachebartm00s@proton.me>
2025-02-17 01:05:23 +0000

    better death handling, prevent race conditions

--- routines.c
+++ routines.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 19:45:37 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/16 22:25:04 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/17 00:57:50 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,21 +62,14 @@ void	*philosopher_routine(void *arg)
 		usleep(1000);
 	while (!check_if_simulation_should_stop(philo))
 	{
-		if (get_elapsed_time(philo->data.last_meal_time)
-			> philo->data.time_to_die)
-		{
-			print_message(&philo->data, "died");
-			stop_simulation(philo);
+		if (check_and_handle_death(philo))
 			return (NULL);
-		}
 		if (try_pick_forks(philo))
 		{
-			if (get_elapsed_time(philo->data.last_meal_time) > philo->data.time_to_die)
+			if (check_and_handle_death(philo))
 			{
-				print_message(&philo->data, "died");
-				stop_simulation(philo);
 				release_forks(philo);
-				return (NULL);
+				return(NULL);
 			}
 			philosopher_eat(philo);
 			release_forks(philo);
@@ -89,16 +82,12 @@ void	*philosopher_routine(void *arg)
 			if (!check_if_simulation_should_stop(philo))
 			{
 				print_message(&philo->data, "is sleeping");
-				long sleep_start = get_current_time_ms();
+				long	sleep_start = get_current_time_ms();
 				while (get_elapsed_time(sleep_start) < philo->data.time_to_sleep)
 				{
-					if (get_elapsed_time(philo->data.last_meal_time) > philo->data.time_to_die)
-					{
-						print_message(&philo->data, "died");
-						stop_simulation(philo);
+					if (check_and_handle_death(philo))
 						return (NULL);
-					}
-					usleep(1000);
+					usleep(100);
 				}
 				if (!check_if_simulation_should_stop(philo))
 					print_message(&philo->data, "is thinking");
