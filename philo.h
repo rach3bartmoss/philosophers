dd9f7ff0ec2f2eea9af5eb727e94f496c8e4a066
Douglas Alves <rachebartm00s@proton.me>
2025-02-17 01:04:51 +0000

    header file changes

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 21:01:41 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/16 22:24:21 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/17 00:49:04 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,7 @@ void	cleanup_all(t_list *head, pthread_mutex_t *print_message,
 //utils2.c
 bool	check_if_simulation_should_stop(t_list *philo);
 void	stop_simulation(t_list *philo);
+bool	check_and_handle_death(t_list *philo);
 //routine_helper.c
 void	one_philo_handler(t_list *philo);
 
