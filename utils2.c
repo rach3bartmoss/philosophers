4edf3b9564b0b376f8ad6b3cb3718ba390436899
Douglas Alves <rachebartm00s@proton.me>
2025-02-19 01:42:58 +0000

    final tests

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 22:02:40 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/17 00:49:09 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/17 23:15:07 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,39 @@ void	stop_simulation(t_list *philo)
 
 bool	check_and_handle_death(t_list *philo)
 {
+	long	current_time;
+	bool	is_dead = false;
+
+	current_time = get_elapsed_time(philo->data.last_meal_time);
+	if (current_time > philo->data.time_to_die)
+	{
+		pthread_mutex_lock(philo->data.stop_mutex);
+		if (get_elapsed_time(philo->data.last_meal_time) > philo->data.time_to_die
+				&& !*philo->data.simulation_stop)
+		{
+			*philo->data.simulation_stop = true;
+			is_dead = true;
+			pthread_mutex_unlock(philo->data.stop_mutex);
+			pthread_mutex_lock(philo->data.print_message);
+			printf("%ld %d %s\n",
+				get_elapsed_time(philo->data.start_time_ms),
+				philo->data.philo_id,
+				"died");
+			//print_message(&philo->data, "died");
+			pthread_mutex_unlock(philo->data.print_message);
+		}
+		else
+		{
+			pthread_mutex_unlock(philo->data.stop_mutex);
+		}
+	}
+	return (is_dead);
+}
+
+
+/*
+bool	check_and_handle_death(t_list *philo)
+{
 	if (get_elapsed_time(philo->data.last_meal_time) > philo->data.time_to_die)
 	{
 		pthread_mutex_lock(philo->data.stop_mutex);
@@ -51,4 +84,4 @@ bool	check_and_handle_death(t_list *philo)
 		return (true);
 	}
 	return (false);
-}
+}*/
