c8417a16776222ef3d2d33ea8041636c5ef6dbc1
Douglas Alves <rachebartm00s@proton.me>
2025-02-22 16:38:07 +0000

    stable version 2.0 optimizations

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 22:02:40 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/17 23:15:07 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/02/19 23:36:46 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,19 @@ void	stop_simulation(t_list *philo)
 	pthread_mutex_unlock(philo->data.stop_mutex);
 }
 
-bool	check_and_handle_death(t_list *philo)
+/*bool	check_and_handle_death(t_list *philo)
 {
 	long	current_time;
-	bool	is_dead = false;
+	bool	is_dead;
 
+	is_dead = false;
 	current_time = get_elapsed_time(philo->data.last_meal_time);
 	if (current_time > philo->data.time_to_die)
 	{
 		pthread_mutex_lock(philo->data.stop_mutex);
-		if (get_elapsed_time(philo->data.last_meal_time) > philo->data.time_to_die
-				&& !*philo->data.simulation_stop)
+		if (get_elapsed_time(philo->data.last_meal_time)
+			> philo->data.time_to_die
+			&& !*philo->data.simulation_stop)
 		{
 			*philo->data.simulation_stop = true;
 			is_dead = true;
@@ -53,35 +55,10 @@ bool	check_and_handle_death(t_list *philo)
 				get_elapsed_time(philo->data.start_time_ms),
 				philo->data.philo_id,
 				"died");
-			//print_message(&philo->data, "died");
 			pthread_mutex_unlock(philo->data.print_message);
 		}
 		else
-		{
 			pthread_mutex_unlock(philo->data.stop_mutex);
-		}
 	}
 	return (is_dead);
-}
-
-
-/*
-bool	check_and_handle_death(t_list *philo)
-{
-	if (get_elapsed_time(philo->data.last_meal_time) > philo->data.time_to_die)
-	{
-		pthread_mutex_lock(philo->data.stop_mutex);
-		if (!*philo->data.simulation_stop)
-		{
-			*philo->data.simulation_stop = true;
-			pthread_mutex_unlock(philo->data.stop_mutex);
-			print_message(&philo->data, "died");
-		}
-		else
-		{
-			pthread_mutex_unlock(philo->data.stop_mutex);
-		}
-		return (true);
-	}
-	return (false);
 }*/
