7b7f3255585796c47691bd64f6b95c119e823952
Douglas Alves <rachebartm00s@proton.me>
2025-03-01 05:23:16 +0000

    norminette

--- routines_helper.c
+++ routines_helper.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 22:20:20 by dopereir          #+#    #+#             */
-/*   Updated: 2025/02/26 23:40:44 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/03/01 05:17:08 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,6 @@ void	stop_simulation(t_list *philo)
 	pthread_mutex_lock(philo->data.stop_mutex);
 	*philo->data.simulation_stop = true;
 	pthread_mutex_unlock(philo->data.stop_mutex);
-}
-
-bool	check_if_simulation_should_stop(t_list *philo)
-{
-	bool	should_stop;
-
-	if (!philo || !philo->data.simulation_stop || !philo->data.stop_mutex)
-		return (true);
-	pthread_mutex_lock(philo->data.stop_mutex);
-	should_stop = *philo->data.simulation_stop;
-	pthread_mutex_unlock(philo->data.stop_mutex);
-	return (should_stop);
 }
 
 bool	helper_pick_forks(t_list *philo)
@@ -79,4 +67,16 @@ bool	helper_pick_forks_rev(t_list *philo)
 	}
 	print_message(&philo->data, "has taken a fork");
 	return (true);
+}
+
+int	helper_philo_eat(t_list *philo)
+{
+	if (check_if_simulation_should_stop(philo))
+		return (0);
+	philo->data.n_of_times_philos_eat--;
+	if (philo->data.n_of_times_philos_eat != 0)
+		print_message(&philo->data, "is sleeping");
+	if (philo->data.n_of_times_philos_eat == 0)
+		return (0);
+	return (1);
 }
