34be7383bbe9bba29619162fd525e20e0ebe7a0f
Douglas Alves <rachebartm00s@proton.me>
2025-03-03 19:36:37 +0000

    stable version, deploy

--- routines_helper.c
+++ routines_helper.c
@@ -6,7 +6,7 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 22:20:20 by dopereir          #+#    #+#             */
-/*   Updated: 2025/03/01 05:17:08 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:07:45 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,40 +29,20 @@ void	stop_simulation(t_list *philo)
 	pthread_mutex_unlock(philo->data.stop_mutex);
 }
 
-bool	helper_pick_forks(t_list *philo)
+bool	helper_pick_forks(t_list *philo, t_list *f_fork, t_list *s_fork)
 {
-	pthread_mutex_lock(&philo->fork);
+	pthread_mutex_lock(&f_fork->fork);
 	if (check_if_simulation_should_stop(philo))
 	{
-		pthread_mutex_unlock(&philo->fork);
+		pthread_mutex_unlock(&f_fork->fork);
 		return (false);
 	}
 	print_message(&philo->data, "has taken a fork");
-	pthread_mutex_lock(&philo->prev->fork);
+	pthread_mutex_lock(&s_fork->fork);
 	if (check_if_simulation_should_stop(philo))
 	{
-		pthread_mutex_unlock(&philo->fork);
-		pthread_mutex_unlock(&philo->prev->fork);
-		return (false);
-	}
-	print_message(&philo->data, "has taken a fork");
-	return (true);
-}
-
-bool	helper_pick_forks_rev(t_list *philo)
-{
-	pthread_mutex_lock(&philo->prev->fork);
-	if (check_if_simulation_should_stop(philo))
-	{
-		pthread_mutex_unlock(&philo->prev->fork);
-		return (false);
-	}
-	print_message(&philo->data, "has taken a fork");
-	pthread_mutex_lock(&philo->fork);
-	if (check_if_simulation_should_stop(philo))
-	{
-		pthread_mutex_unlock(&philo->prev->fork);
-		pthread_mutex_unlock(&philo->fork);
+		pthread_mutex_unlock(&f_fork->fork);
+		pthread_mutex_unlock(&s_fork->fork);
 		return (false);
 	}
 	print_message(&philo->data, "has taken a fork");
@@ -79,4 +59,22 @@ int	helper_philo_eat(t_list *philo)
 	if (philo->data.n_of_times_philos_eat == 0)
 		return (0);
 	return (1);
+}
+
+void	helper_routine_sync(t_list *philo)
+{
+	long	time_since_last_meal;
+	long	time_until_death;
+	long	thinking_time;
+
+	print_message(&philo->data, "is thinking");
+	time_since_last_meal = get_current_time_ms() - philo->data.last_meal_time;
+	time_until_death = philo->data.time_to_die - time_since_last_meal;
+	if (time_until_death > philo->data.time_to_die / 2)
+	{
+		thinking_time = (philo->data.time_to_eat * 100);
+		usleep(thinking_time);
+	}
+	else if (time_until_death > philo->data.time_to_die / 4)
+		usleep(500);
 }
